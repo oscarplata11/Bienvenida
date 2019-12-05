fb2c623a17b2a2f408442b93e12df9ac8fd5cf04
unknown <oscarplata1157@gmail.com>
2019-12-05 11:22:00 +0100

    Se modifica saludo.js

--- saludo.js
+++ saludo.js
@@ -1,3 +1,5 @@
-var saludo=prompt("Por favor, escriba su nombre");
+var nombre=prompt("Por favor, escriba su nombre");
+var apellido1=prompt("Por favor, escriba su primer apellido");
+var apellido2=prompt("Por favor, escriba su segundo apellido");
 
-document.write("Hola " + saludo);
+document.write("Hola " + nombre + " " + apellido1 + " " + apellido2);
