f2631c88d792bd161373cb9defdad3958c371608
unknown <oscarplata1157@gmail.com>
2019-12-05 11:24:33 +0100

    Se modifica saludo.js

--- saludo.js
+++ saludo.js
@@ -1,5 +1,6 @@
 var nombre=prompt("Por favor, escriba su nombre");
 var apellido1=prompt("Por favor, escriba su primer apellido");
 var apellido2=prompt("Por favor, escriba su segundo apellido");
+var dni=prompt("Por favor, introduzca su DNI");
 
-document.write("Hola " + nombre + " " + apellido1 + " " + apellido2);
+document.write("Hola " + nombre + " " + apellido1 + " " + apellido2 + ", con DNI " + dni);
